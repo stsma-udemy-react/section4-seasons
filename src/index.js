7c9ae47df579c10c6ca8ebdc51811361a32efd13
stephensmakilan <stephensmakilan@gmail.com>
2021-02-17 11:50:03 +0800

    End of Section 4

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//const App = () =>{
+//    window.navigator.geolocation.getCurrentPosition( 
+//        (position) => console.log(position),
+//        (err) => console.log(err)
+//    )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//    return(
+//        <div>
+//            Hi there!
+//        </div>
+//    )
+//}
+
+class App extends React.Component{
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (error) => console.log(error)
+        );
+
+        return(
+            <div>Hi there!</div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
